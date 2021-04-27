014ffebc83582e10e79b46dcee4f3d769712813e
Kirill Ivashchenko <strategovich@gmail.com>
2021-04-27 20:05:24 +0300

    update

---
+++ blanchard/js/effects.js
@@ -0,0 +1,202 @@
+$(document).ready(function(){
+  $("#header-nav-menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1000);
+  });
+});
+
+$(document).ready(function(){
+  document.querySelectorAll('.header__dropdown-menu-name').forEach(function (elem) {
+    elem.addEventListener('click', function (event) {
+      const menu = event.currentTarget.dataset.menu
+
+      if (document.querySelector(`[data-list="${menu}"]`).classList.contains('header__dropdown-visible')) {
+        document.querySelector('.header__dropdown-visible').classList.remove('header__dropdown-visible');
+      } else {
+        document.querySelectorAll('.header__dropdown-item').forEach(function (hdi) {
+          hdi.classList.remove('header__dropdown-visible');
+        });
+        document.querySelector(`[data-list="${menu}"]`).classList.add('header__dropdown-visible');
+      }
+    })
+  })
+});
+
+$( ".catalogue__accordion-active" ).accordion();
+
+$(document).ready(function(){
+  document.querySelectorAll('.catalogue__country-button').forEach(function (elem) {
+    elem.addEventListener('click', function (event) {
+      const country = event.currentTarget.dataset.country
+
+      document.querySelectorAll('.catalogue__country-button').forEach(function (button) {
+        button.classList.remove('catalogue__country-button-active')
+      })
+      event.currentTarget.classList.add('catalogue__country-button-active')
+
+      document.querySelectorAll('.catalogue__accordion-active').forEach(function (accordion1) {
+        accordion1.classList.remove('catalogue__accordion-active')
+      })
+      document.querySelector(`[data-accord="${country}"]`).classList.add('catalogue__accordion-active')
+
+      $( ".catalogue__accordion-active" ).accordion();
+
+      document.querySelectorAll('.catalogue__accordion-list-item').forEach(function (listitem) {
+        listitem.classList.remove('catalogue__accordion-list-item-active')
+      })
+      document.querySelectorAll('.catalogue__artist-page').forEach(function (artistpage) {
+        artistpage.classList.remove('catalogue__artist-page-active')
+      })
+      document.querySelector('.catalogue__artist-page-blank').classList.add('catalogue__artist-page-active')
+    })
+  })
+  document.querySelectorAll('.catalogue__accordion-list-item').forEach(function (artistpage) {
+    artistpage.addEventListener('click', function (event) {
+      const number = event.currentTarget.dataset.artistlink
+
+      document.querySelectorAll('.catalogue__accordion-list-item').forEach(function (listitem) {
+        listitem.classList.remove('catalogue__accordion-list-item-active')
+      })
+      event.currentTarget.classList.add('catalogue__accordion-list-item-active')
+
+      document.querySelectorAll('.catalogue__artist-page').forEach(function (artistpage) {
+        artistpage.classList.remove('catalogue__artist-page-active')
+      })
+      document.querySelector(`[data-artistpage="${number}"]`).classList.add('catalogue__artist-page-active')
+    })
+  })
+});
+
+const swiper_hero = new Swiper('.swiper-container-hero', {
+  speed: 2000,
+  allowTouchMove: false,
+  effect: 'fade',
+  loop:true,
+  autoplay: {
+    delay: 3500,
+  },
+});
+
+const swiper_gallery = new Swiper('.swiper-container-gallery', {
+  speed: 500,
+  spaceBetween: 50,
+  allowTouchMove: false,
+  effect: 'slide',
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  slidesPerColumn: 2,
+  navigation: {
+    nextEl: '.gallery__swiper-button-next',
+    prevEl: '.gallery__swiper-button-prev',
+  },
+  pagination: {
+    el: '.gallery__swiper-pagination',
+    type: 'fraction',
+  },
+});
+
+const swiper_books = new Swiper('.swiper-container-books', {
+  speed: 500,
+  spaceBetween: 50,
+  allowTouchMove: false,
+  effect: 'slide',
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  navigation: {
+    nextEl: '.books__swiper-button-next',
+    prevEl: '.books__swiper-button-prev',
+  },
+  pagination: {
+    el: '.books__swiper-pagination',
+    type: 'fraction',
+  },
+});
+
+const swiper_projects = new Swiper('.swiper-container-projects', {
+  speed: 500,
+  spaceBetween: 50,
+  allowTouchMove: false,
+  loop: true,
+  effect: 'slide',
+  slidesPerView: 3,
+  navigation: {
+    nextEl: '.projects__swiper-button-next',
+    prevEl: '.projects__swiper-button-prev',
+  },
+});
+
+ymaps.ready(init);
+
+function init () {
+    myMap = new ymaps.Map('map', {
+        center: [55.755, 37.610],
+        zoom: 15
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+    var myPlacemark = new ymaps.Placemark([55.758458,37.600995], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '/img/map-pin.svg',
+      iconImageSize: [20, 20],
+      iconImageOffset: [-10, -10]
+    });
+    myMap.geoObjects.add(myPlacemark);
+};
+
+function opencards () {
+  document.querySelectorAll('.events__card').forEach(function(card) {
+    card.classList.add('events__card-visible');
+  });
+  document.querySelector('.events__more-button').classList.add('events__more-button-hidden');
+  document.querySelector('.events__card-list').classList.add('events__card-list-full')
+};
+
+function inputfocus () {
+  $(".header__search").focus()
+}
+
+function slidetomap () {
+  $('body,html').animate({scrollTop: top = $("#contacts-section").offset().top}, 1000);
+}
+
+var selector1 = document.querySelector(".phone-input");
+
+var im1 = new Inputmask("+7(999) 999-99-99");
+im1.mask(selector1);
+
+const choices = new Choices('.gallery__filters-author', {
+  shouldSort: false,
+  itemSelectText: '',
+  searchEnabled: false,
+  position: 'bottom',
+});
+
+new window.JustValidate('.contacts__form', {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 30,
+    },
+    phone: {
+      required: true,
+      function: (name, value) => {
+        const phone = selector1.inputmask.unmaskedvalue();
+        return Number(phone) && phone.length === 10;
+      }
+    }
+  },
+  messages: {
+    phone: {
+      required: 'Укажите ваш телефон',
+      function: 'Укажите ваш телефон',
+    },
+    name: {
+      required: 'Как вас зовут?',
+      minLength: 'Слишком коротко',
+      maxLength: 'Слишком длинное',
+    }
+  },
+});
