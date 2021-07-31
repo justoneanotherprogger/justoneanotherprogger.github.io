98e9b152a7d1cf061e892f4c2451141687c90583
Kirill Ivashchenko <strategovich@gmail.com>
2021-07-31 20:06:11 +0300

    Grand update

---
+++ js/effects.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("#header-nav-menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1000);
+  });
+});
+
+$(document).ready(function(){
+  document.querySelector('.about__diploma').addEventListener('click', function (event) {
+    document.querySelector('.about__modal-diploma').classList.toggle('visible');
+  });
+  document.querySelector('.about__modal-diploma-background').addEventListener('click', function (event) {
+    document.querySelector('.about__modal-diploma').classList.toggle('visible');
+  });
+  document.querySelector('.about__modal-close').addEventListener('click', function (event) {
+    document.querySelector('.about__modal-diploma').classList.toggle('visible');
+  });
+});
+
+$(document).ready(function(){
+
+});
