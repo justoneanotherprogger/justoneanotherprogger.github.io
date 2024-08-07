75f2c8f85cea061f7771a60ba4e8b666a0c7d389
Kirill Ivashchenko <strategovich@gmail.com>
2024-08-07 08:48:07 +0300

    Еще раз сменил подход к липкому меню

--- js/effects.js
+++ js/effects.js
@@ -12,14 +12,3 @@ $(document).ready(function(){
     $('body,html').animate({scrollTop: top}, 500);
   });
 });
-
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var distanceNav = $('#about-start').offset().top;
-    var fixedelem = document.getElementById('main-header')
-    if ($(window).scrollTop() >= distanceNav)
-      fixedelem.classList.add('fixed-on-top');
-    else
-      fixedelem.classList.remove('fixed-on-top');
-  });
-});
