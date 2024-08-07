6b541addb3567f16cbaeaec0d6df785c295c810e
Kirill Ivashchenko <strategovich@gmail.com>
2024-08-07 08:52:38 +0300

    Чуть скорректировал настройки

--- js/effects.js
+++ js/effects.js
@@ -5,10 +5,4 @@ $(document).ready(function(){
     top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 500);
   });
-  $("#fixed-nav-menu").on("click","a", function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href'),
-    top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 500);
-  });
 });
