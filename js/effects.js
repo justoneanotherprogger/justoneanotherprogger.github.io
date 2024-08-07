b7d395b0b5570840d5c5ea84cd1bc74e35348382
Kirill Ivashchenko <strategovich@gmail.com>
2024-08-07 07:41:16 +0300

    Попытка добавить липкое меню

--- js/effects.js
+++ js/effects.js
@@ -5,4 +5,20 @@ $(document).ready(function(){
     top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 500);
   });
+  $("#fixed-nav-menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 500);
+  });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var distanceNav = $('#header-nav-menu').offset().top;
+    if ($(window).scrollTop() >= distanceNav)
+      $('#fixed-nav-menu').show();
+    else
+      $('#fixed-nav-menu').hide();
+  });
 });
