ebb189b41c9ae024db8c3f65473e73bb045af097
Kirill Ivashchenko <strategovich@gmail.com>
2024-08-08 11:09:18 +0300

    Пара улучшений

--- js/effects.js
+++ js/effects.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+  var height = document.getElementById('header-main').offsetHeight;
+
   $("#header-nav-menu").on("click","a", function (event) {
     event.preventDefault();
     var id  = $(this).attr('href'),
-    top = $(id).offset().top;
+    top = $(id).offset().top  + height;
     $('body,html').animate({scrollTop: top}, 500);
   });
 });
